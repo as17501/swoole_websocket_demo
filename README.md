d6e5c7207efe2a39f64ea95871291abb0a5ee9f4
longmao <6158731@163.com>
2016-06-30 09:34:51 +0800

    修改数据库调用和自动关闭

--- README.md
+++ README.md
@@ -5,3 +5,8 @@
 返回参数
  {"command":"poker_sit","params":{"a":"b"},"retrundata":{"a":"b"}}
 说明：  发送者调用的控制器_方法，发送者调用的参数array,服务器返回的参数
+
+数据库调用
+$db = $this->db;
+
+data文件夹存放的是数据库日志，只保存了出错的sql
--- app/controllers/baseController.php
+++ app/controllers/baseController.php
@@ -2,9 +2,12 @@
 class baseController{
 	public $ws;
 	public $fd;
+	public $db;
 	function __construct($ws,$fd){
 		$this->ws = $ws;
 		$this->fd = $fd;
+		$this->db = new db();
+
 		
  	}
  	//参数出错
@@ -12,22 +15,8 @@ class baseController{
  		$this->ws->push($this->fd, json_encode('params error!!!'));
  		exit();
  	}
- 	//读取用户信息
- 	function load_user($user = ""){
- 		if(!$user){
- 			$u = $this->ws->user->get($this->fd);
- 			$user =  $db->getfirst($db->query("select a.*,b.* from user as a
-								left join userinfo as b on a.id = b.uid
-								where a.id='".$u['id']."' "));
- 		}
- 		return  $this->ws->user->set($this->fd, array('id' => $user["id"], 'username' => $user["username"], 'gold' =>$user["gold"], 'diamonds' =>$user["diamonds"], 'glamour' =>$user["glamour"], 'max_win' =>$user["max_win"], 'all_win' =>$user["all_win"], 'all_played' =>$user["all_played"], 'bet_time' =>$user["bet_time"], 'win_time' =>$user["win_time"], 'showdown_time' =>$user["showdown_time"], 'sex' =>$user["sex"],'show_pic'=>$user["show_pic"] ));
- 	}
- 	
- 	//保存用户信息
- 	function save_user(){
- 		$db = new db();
- 		$user = $this->ws->user->get($this->fd);
- 		return $db->update("userinfo", "username ='".$user['username']."',gold ='".$user['gold']."',diamonds ='".$user['diamonds']."',glamour ='".$user['glamour']."',max_win ='".$user['max_win']."',all_played ='".$user['all_played']."',bet_time ='".$user['bet_time']."',win_time ='".$user['win_time']."',showdown_time ='".$user['showdown_time']."',sex ='".$user['sex']."',show_pic ='".$user['show_pic']."'", "uid = ".$user['id']); 		
+ 	function __destruct(){
+ 		$this->db->dbClose();
  	}
 }
 
